49d2ed1456110e212de6b33963cda3446bc69e0f
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2018-02-12 15:17:16 +0900

    Add value mapping.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-export const valueOf = x => 2 ** ((x - 63) / 6);
+export const valueOf = (() => {
+    const is = Array.from({length: 126}).map((_, i) => i);
+    const values = [].concat(
+        NaN,
+        -Infinity,
+        is.map(i => -1 * 2 ** ((62 - i) / 6)),
+        0,
+        is.map(i => 2 ** ((i - 63) / 6)),
+        Infinity,
+    );
+    return x => values[x + 128];
+})();
--- test/index.js
+++ test/index.js
@@ -1,6 +1,32 @@
-import { valueOf } from '../index';
-import { equal as assertEq } from 'assert';
+import {
+  valueOf
+} from '../index';
+import {
+  equal as assertEq
+} from 'assert';
 
-describe('foo', () => {
-  it('blar', () => assertEq(1, valueOf(63)));
+describe('rough values', () => {
+  it('#0 is zero.', () => assertEq(valueOf(0), 0));
+  it('#64 is one.', () => assertEq(valueOf(64), 1));
+  it('#127 is Infinity.', () => assertEq(valueOf(127), Infinity));
+  it('#-64 is -1.', () => assertEq(valueOf(-64), -1));
+  it('#-127 is -Infinity.', () => assertEq(valueOf(-127), -Infinity));
+  it('#-128 is NaN.', () => assertEq(Number.isNaN(valueOf(-128)), true));
+  it('size is same and its sign is opposite for respective index of [1, 127).', () => {
+    for (let i = 1; i < 127; ++i) {
+      assertEq(valueOf(i) + valueOf(-i), 0);
+    }
+  });
+  it('absolute(valueOf(#-1)) === valueOf(#1)', () => assertEq(Math.abs(valueOf(-1)), valueOf(1)));
+  it('#7 is just 2 times bigger than #1.', () => assertEq(valueOf(7), 2 * valueOf(1)));
+  it('#13 is just 4 times bigger than #1.', () => assertEq(valueOf(13), 4 * valueOf(1)));
+  it('#19 is just 8 times bigger than #1.', () => assertEq(valueOf(19), 8 * valueOf(1)));
+  it('#25 is just 16 times bigger than #1.', () => assertEq(valueOf(25), 16 * valueOf(1)));
+  it('#31 is just 32 times bigger than #1.', () => assertEq(valueOf(31), 32 * valueOf(1)));
+  it('#37 is just 64 times bigger than #1.', () => assertEq(valueOf(37), 64 * valueOf(1)));
+  it('#43 is just 128 times bigger than #1.', () => assertEq(valueOf(43), 128 * valueOf(1)));
+  it('#49 is just 256 times bigger than #1.', () => assertEq(valueOf(49), 256 * valueOf(1)));
+  it('#55 is just 512 times bigger than #1.', () => assertEq(valueOf(55), 512 * valueOf(1)));
+  it('#61 is just 1024 times bigger than #1.', () => assertEq(valueOf(61), 1024 * valueOf(1)));
+  it('#67 is just square-root times bigger than #64.', () => assertEq(valueOf(67), Math.SQRT2 * valueOf(64)));
 });
